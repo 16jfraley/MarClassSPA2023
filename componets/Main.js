c45e91f5031c7a696a772c12b7cebd4004280274
Josh Fraley <fraley.j3@gmail.com>
2023-04-26 21:21:16 -0500

    SPA state

--- componets/Main.js
+++ componets/Main.js
@@ -2,9 +2,4 @@
 import html from "html-literal";
 import * as views from "./veiws";
 
-export default () => html`
-  ${views["Home"]()},
-  ${views["Bio"]()},
-  ${views["Pizza"]()},
-  ${views["Order"]()}
-`;
+export default (state) => html` ${views[state.view](state)} `;
--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 import {Header, Nav, Main, Footer} from "./componets";
+import * as store from "./store";
 
+import Navigo from "navigo";
+import { capitalize } from "lodash";
 
-function render() {
+const router = new Navigo("/");
+
+function render(state = store.Home) {
   document.querySelector('#root').innerHTML = `
-  ${Header()}
-  ${Nav()}
-  ${Main()}
+  ${Header(state)}
+  ${Nav(store.Links)}
+  ${Main(state)}
   ${Footer()}`;
+
+  afterRender(state);
+
+  router.updatePageLinks();
 }
 
-render();
+
+
+
+
+
 // add menu toggle to bars icon in nav bar
 
-// document.querySelector(".fa-bars").addEventListener("click", () => {
-//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-// });
+function afterRender(state) {
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
+}
+
+
+router
+.on({
+  "/": () => render(),
+  ":view": (params) => {
+    let view = capitalize(params.data.view);
+    if (store.hasOwnProperty(view)) {
+      render(store[view]);
+    } else {
+      console.log(`View ${view} not defined`);
+    }
+  },
+})
+.resolve();
